237c9d33f2b5f5824e4872ddd83cc44dff58cd92
amirdianov <amirdianov@gmail.com>
2022-04-24 01:13:13 +0300

    done db_func

--- db_func.py
+++ db_func.py
@@ -12,30 +12,26 @@ conn = sqlite3.connect('library.sqlite', check_same_thread=False)
 cursor = conn.cursor()
 
 
-# doesn't work now
-def add_item(self, item_text, owner):
-    stmt = "INSERT INTO items (description, owner) VALUES (?, ?)"
-    args = (item_text, owner)
-    cursor.execute(stmt, args)
-    conn.commit()
-
-
-# doesn't work now
-def delete_item(self, item_text, owner):
-    stmt = "DELETE FROM items WHERE description = (?) AND owner = (?)"
-    args = (item_text, owner)
-    cursor.execute(stmt, args)
+# work
+# you don't need to touch this method
+def add_item(user_id):
+    cursor.execute("INSERT INTO Users (telegram_id, name_surname,subscription,registration) VALUES (?,?,?,?)",
+                   (user_id, None, False, False))
+    cursor.execute("INSERT INTO subscription (telegram_id, valid_until,  exists_since, activity) VALUES (?,?,?,?)",
+                   (user_id, None, None, False))
     conn.commit()
 
 
 # work
 # url = [x[0] for x in cursor.execute('SELECT url from Books WHERE title = (?)', (book,))]
 def get_items(from_which_column, table, column_where, value):
+    '''return list values with value'''
     stmt = f'SELECT {from_which_column} FROM {table} WHERE {column_where} = (?)'
     args = (value,)
     return [x[0] for x in cursor.execute(stmt, args)]
 
 
-# doesn't work now
-def update_items():
-    pass
+# work
+# cursor.execute('UPDATE Persons SET user_register = ? WHERE user_id = ?', (us_adress, us_id))
+def update_items(table, what_past, where_past, value_past, where_value):
+    cursor.execute(f'UPDATE {table} SET {what_past} = ? WHERE {where_past} = ?', (value_past, where_value))
