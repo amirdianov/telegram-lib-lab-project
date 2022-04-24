8737fc80f92b7a2d4234bbecf85302b7d9fca702
bulatsabirow <mlbulat@mail.ru>
2022-04-24 13:47:51 +0300

    first attempt to realise registration

--- bot.py
+++ bot.py
@@ -52,8 +52,14 @@ class User:
         self.registration = registration
         self.subscription = subscription
 
-    def registration_func(self: Update, context: Any):
-        registration_user(self, context)
+    #def registration_func(self: Update, context: Any):
+    #    registration_user(self, context)
+
+    def begin_registration_user_func(self: Update, context: Any):
+        return begin_registration_user(self, context)
+
+    def handle_user_data_func(self: Update, context: Any):
+        return handle_user_data(self, context)
 
     def subscription_func(self: Update, context: Any):
         subscription_user(self, context)
@@ -106,16 +112,27 @@ def main() -> None:
 
     dispatcher.add_handler(CommandHandler('methods', methods_func))
     dispatcher.add_handler(PrefixHandler('❓', 'help', help_func))
-    dispatcher.add_handler(PrefixHandler('💻', 'registration', User.registration_func))
+    #dispatcher.add_handler(PrefixHandler('💻', 'registration', User.registration_func))
     dispatcher.add_handler(PrefixHandler('📅', 'subscription', User.subscription_func))
     conv_handler = ConversationHandler(
-        entry_points=[PrefixHandler('📖', 'take_book', User.take_book_func)],
+        entry_points=[PrefixHandler('📖', 'take_book',User.take_book_func)],
         states={
             1: [MessageHandler(Filters.text, User.take_book_1_func, pass_user_data=True)]
         }, fallbacks=[CommandHandler('stop', stop)]
     )
     dispatcher.add_handler(conv_handler)
+
     dispatcher.add_handler(MessageHandler(Filters.text, command, pass_user_data=True))
+
+    conv_handler_registration = ConversationHandler(entry_points=
+    [PrefixHandler('💻', 'registration', User.begin_registration_user_func)],
+    states=
+    {1: [MessageHandler(Filters.text, User.handle_user_data_func)]
+    },
+    fallbacks=[CommandHandler('stop', stop)])
+
+    dispatcher.add_handler(conv_handler_registration)
+
     updater.start_polling()
 
     updater.idle()
--- registration.py
+++ registration.py
@@ -17,8 +17,21 @@ def check_registration():
     #     return False
     return False
 
-
-def registration_user(self: Update, context: Any):
+#def registration_user(self: Update, context: Any):
+def begin_registration_user(self: Update, context: Any):
     """make people to registred and insert to db"""
     """use ConversationHandler to insert user for db"""
-    self.message.reply_text('Давайте регистрироваться. Введите свои данные')
+    if check_registration():
+        self.message.reply_text('Вы уже зарегистрированы в системе👍')
+        return ConversationHandler.END
+    else:
+        self.message.reply_text('Давайте регистрироваться. Введите свои данные')
+        return 1
+
+def handle_user_data(self: Update, context: Any):
+    """handle user data and insert it into db"""
+    context.user_data['name_surname']: str = update.message.text.strip()
+    update_items('users', 'name_surname', 'telegram_id', context.user_data['name_surname'], self.message.from_user.id)
+    self.message.reply_text('Вы успешно зарегистровались ✅')
+    return ConversationHandler.END
+
