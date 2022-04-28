ad748e7d4313f3603cb4e1e9c45f1d829a7cae33
diyanovamir <amirdianov@gmail.com>
2022-04-28 21:58:34 +0300

    start of subscription method

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 """
 Telegram bot LibLab
 """
+import time
 
 from registration import *
 from subscription import *
@@ -13,7 +14,7 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
-TOKEN: str = '5153990837:AAHVrwUUYPFwfQlGv37TeZ2A3dsW1MYWRis'
+TOKEN: str = '5224259246:AAFNi4jQBZ19CSuiqkB9kYNw6mz6h-lqI7E'
 
 
 def start_messaging(update: Update, context: Any) -> int:
@@ -62,12 +63,22 @@ class User:
     def registration_handle_user_data_func(self: Update, context: Any):
         flag: bool = registration_handle_user_data(self, context)
         if flag:
+            time.sleep(2)
             methods_func(self, context)
             return ConversationHandler.END
         return 1
 
-    def subscription_func(self: Update, context: Any):
-        subscription_user(self, context)
+    def begin_subscription_user_func(self: Update, context: Any):
+        flag: bool = subscription_user(self, context)
+        if not flag:
+            time.sleep(2)
+            methods_func(self, context)
+            return ConversationHandler.END
+        else:
+            return 1
+
+    def subscription_handle_user_func(self: Update, context: Any):
+        ...
 
     def take_book_func(self: Update, context: Any):
         take_book_user(self, context)
@@ -117,10 +128,8 @@ def main() -> None:
 
     dispatcher.add_handler(CommandHandler('methods', methods_func))
     dispatcher.add_handler(PrefixHandler('❓', 'help', help_func))
-    #dispatcher.add_handler(PrefixHandler('💻', 'registration', User.begin_registration_user_func))
-    dispatcher.add_handler(PrefixHandler('📅', 'subscription', User.subscription_func))
     conv_handler = ConversationHandler(
-        entry_points=[PrefixHandler('📖', 'take_book',User.take_book_func)],
+        entry_points=[PrefixHandler('📖', 'take_book', User.take_book_func)],
         states={
             1: [MessageHandler(Filters.text, User.take_book_1_func, pass_user_data=True)]
         }, fallbacks=[CommandHandler('stop', stop)]
@@ -130,12 +139,19 @@ def main() -> None:
     conv_handler_registration = ConversationHandler(
         entry_points=[PrefixHandler('💻', 'registration', User.begin_registration_user_func)],
         states={
-        1: [MessageHandler(Filters.text, User.registration_handle_user_data_func, pass_user_data=True)]
+            1: [MessageHandler(Filters.text, User.registration_handle_user_data_func, pass_user_data=True)]
         }, fallbacks=[CommandHandler('stop', stop)])
-
     dispatcher.add_handler(conv_handler_registration)
-    dispatcher.add_handler(MessageHandler(Filters.text, command, pass_user_data=True))
 
+    conv_handler_subscription = ConversationHandler(
+        entry_points=[PrefixHandler('📅', 'subscription', User.begin_registration_user_func)],
+        states={
+            1: [MessageHandler(Filters.text, User.registration_handle_user_data_func, pass_user_data=True)]
+        }, fallbacks=[CommandHandler('stop', stop)]
+    )
+    dispatcher.add_handler(conv_handler_subscription)
+
+    dispatcher.add_handler(MessageHandler(Filters.text, command, pass_user_data=True))
     updater.start_polling()
 
     updater.idle()
--- subscription.py
+++ subscription.py
@@ -1,3 +1,8 @@
+
+
+
+
+
 import logging
 import sqlite3
 import time
@@ -6,13 +11,17 @@ from typing import Any
 from telegram import Update, ForceReply, ReplyKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, ConversationHandler, MessageHandler, CommandHandler, Filters, \
     CallbackContext, PrefixHandler
+from db_func import get_items
 
 from registration import check_registration
 
 
-def find_subscription():
+def activated_subscription(user_id):
     """use db, u can make this method not static, how u can - make"""
-    return True
+    if get_items('activity', 'subscription', 'telegram_id', user_id)[0] == 1:
+        return True
+    else:
+        return False
 
 
 def renew_dates_user(self: Update, context: CallbackContext):
@@ -22,7 +31,18 @@ def renew_dates_user(self: Update, context: CallbackContext):
 
 def subscription_user(self: Update, context: Any):
     "take information from db and make object of Subscription class"
-    if not check_registration():  # обязательная проверка на зарегестрированность
-        check_registration()
+    user_id = self.message.from_user.id
+    if not check_registration(user_id):  # обязательная проверка на зарегистрированность
+        self.message.reply_text('❌ К сожалению, вы не зарегистрированы в нашей системе ❌ \n'
+                                'Пройдите регистрацию для возможности взятия книги 💻')
+        return False
+    else:
+        if activated_subscription(user_id):
+            # ветка наличия подписки
+            ...
+        else:
+            # ветка отсутствия подписки
+            # дописать функцию
+            ...
     # subscription = Subscription(User.find_subscription())
-    self.message.reply_text('Ваша подпска активна или не активна - вопрос')
+    self.message.reply_text('Ваша подписка активна или не активна - вопрос')
